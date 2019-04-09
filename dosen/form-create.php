ba88adc2189a3458f85bf8eb6688a6a0d15d4353
faizdiandra <46486733+faizdiandra@users.noreply.github.com>
2019-04-09 09:23:34 +0700

    Add files via upload

---
+++ dosen/form-create.php
@@ -0,0 +1,19 @@
+<html>
+	<body>
+		<form method="POST" action="create.php">
+			<table>
+			<tr>
+				<td><label for="nama">Nama Dosen</label></td>
+				<td>:</td>
+				<td><input type="text" name="nama_dosen" id="nama"></td>
+			</tr>
+			<tr>
+				<td><label for="no_telp">No. Telepon</td>
+				<td>:</td>
+				<td><input type="text" name="telp" id="no_telp"></td>
+			</tr>
+			</table>
+			<input type="submit" name="btnSimpan" value="simpan">
+		</form>
+	</body>
+</html>
